4a35097d838beda237be5663e3289865e5a45f19
zcabnds <zcabnds@ucl.ac.uk>
2022-02-22 12:37:22 +0000

    Updated Footer.js with updated links for multidashboard project

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -8,7 +8,7 @@ function Footer() {
         className={styles.link}
         target="_blank"
         rel="noopener noreferrer"
-        href="https://comp0016-team-20.github.io/blog/">
+        href="https://carequalitydashboard.wordpress.com/">
         Development Blog
       </a>
       <a
@@ -22,7 +22,7 @@ function Footer() {
         className={styles.link}
         target="_blank"
         rel="noopener noreferrer"
-        href="https://forms.office.com/Pages/ResponsePage.aspx?id=_oivH5ipW0yTySEKEdmlwqQsVN_qV_tIhIq5N2SskKtUNU1XTkZUVlFPUVhJQ0o2UEFQNTM1NVg5Ti4u">
+        href="https://forms.office.com/r/4LW2BdGi9v">
         Share your feedback!
       </a>
       <i>
