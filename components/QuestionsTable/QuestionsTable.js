1c07e4a5e4dd58a485b3738bf62e339c5e7b0a38
zcabnds <zcabnds@ucl.ac.uk>
2022-03-09 22:56:00 +0000

    updated spelling mistake

--- components/QuestionsTable/QuestionsTable.js
+++ components/QuestionsTable/QuestionsTable.js
@@ -333,7 +333,7 @@ export default function QuestionsTable() {
             await deleteDashboard(dashboardId);
           }
         }}>
-        Delete Dashbooard
+        Delete Dashboard
       </Button>
 
       <Button
