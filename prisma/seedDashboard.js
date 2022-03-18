fb5df80de38002b9e47854622f303247c0fe6e44
zcabnds <zcabnds@ucl.ac.uk>
2022-03-18 16:12:44 +0000

    fixed unnecessary return statement for seeding Data

--- prisma/seedDashboard.js
+++ prisma/seedDashboard.js
@@ -136,7 +136,7 @@ const seedDashboards = async userId => {
 const userId = process.argv[2];
 
 if (!userId) {
-  return console.error(
+  console.error(
     'You must run this script with the user ID for whom you want to insert data. e.g. node seedResponses.js fa0c7dea-ade1-4425-c659-4bf56eae7eb6'
   );
 }
